eabdf3144cba02566daf939aff103ca8ccbe0127
Jakob Rødsgaard Jørgensen <mrjakobdk@gmail.com>
2017-04-01 12:54:41 +0200

    Jo im?

--- app/src/main/java/com/example/fevre/fashiondonerightv2/MainActivity.java
+++ app/src/main/java/com/example/fevre/fashiondonerightv2/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.fevre.fashiondonerightv2;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Display;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +44,42 @@ public class MainActivity extends AppCompatActivity {
         int screenHeight = size.y;
 
         FloatingActionButton hatButton = createFab(Math.round(0.5F * screenWidth), 0);
+
+        final MainActivity mainActivity = this;
+
+        hatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FrameLayout selectorMenu = (FrameLayout) findViewById(R.id.selector_menu);
+
+                ResizeWidthAnimation anim = new ResizeWidthAnimation(selectorMenu, 900);
+                anim.setDuration(500);
+                anim.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        Spinner spinner = (Spinner) findViewById(R.id.type_spinner);
+                        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(mainActivity,
+                                R.array.type_array, android.R.layout.simple_spinner_item);
+
+                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                        spinner.setAdapter(adapter);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+                selectorMenu.startAnimation(anim);
+
+            }
+        });
 
 
 
@@ -78,5 +131,75 @@ public class MainActivity extends AppCompatActivity {
         floatingActionButton.setY(y);
 
         return floatingActionButton;
+    }
+
+    public class ResizeWidthAnimation extends Animation
+    {
+        private int mWidth;
+        private int mStartWidth;
+        private View mView;
+
+        public ResizeWidthAnimation(View view, int width)
+        {
+            mView = view;
+            mWidth = width;
+            mStartWidth = view.getWidth();
+        }
+
+        @Override
+        protected void applyTransformation(float interpolatedTime, Transformation t)
+        {
+            int newWidth = mStartWidth + (int) ((mWidth - mStartWidth) * interpolatedTime);
+
+            mView.getLayoutParams().width = newWidth;
+            mView.requestLayout();
+        }
+
+        @Override
+        public void initialize(int width, int height, int parentWidth, int parentHeight)
+        {
+            super.initialize(width, height, parentWidth, parentHeight);
+        }
+
+        @Override
+        public boolean willChangeBounds()
+        {
+            return true;
+        }
+    }
+
+    public class SearchDialogFragment extends DialogFragment implements TextView.OnEditorActionListener {
+
+        private EditText mEditText;
+
+
+        // Empty constructor required for DialogFragment
+        public SearchDialogFragment() {}
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.searchdialog, container);
+            mEditText = (EditText) view.findViewById(R.id.search_editText);
+
+            // set this instance as callback for editor action
+            mEditText.setOnEditorActionListener(this);
+            mEditText.requestFocus();
+            getDialog().getWindow().setSoftInputMode(
+                    WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+            getDialog().setTitle("Please enter username");
+
+            return view;
+        }
+
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            // Return input text to activity
+            /*UserNameListener activity = (UserNameListener) getActivity();
+            activity.onFinishUserDialog(mEditText.getText().toString());
+            this.dismiss();
+            return true;*/
+            return true;
+        }
     }
 }
